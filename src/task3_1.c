6d1706868f99323e5f1fe23feb9d09bcb1cea14e
spm1102 <dovietdung_hsgs19@hus.edu.vn>
2024-03-25 01:07:01 +0700

    [MODIFY] simplize the code

--- src/task3_1.c
+++ src/task3_1.c
@@ -1,13 +1,9 @@
 //Develop a program that receives a real number as input and prints the number to the console.
 #include<stdio.h>
-#include<math.h>
 
 int main(){
-    double a;
-    do{
-        printf("Type a real number with maximum of 6 digits: ");
-        scanf("%lf",&a);
-    } while ((long long)(a * pow(10,6)) != a * pow(10,6));
-    printf("The real number is: %f",a);
+    float a;
+    scanf("%f",&a);
+    printf("%f",a);
     return 0;
 }
