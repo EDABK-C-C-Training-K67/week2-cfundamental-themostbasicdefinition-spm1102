98c72b45d975fa28911b1a4c30560877bafe39de
spm1102 <dovietdung_hsgs19@hus.edu.vn>
2024-03-25 09:48:19 +0700

    [MODIFY] interchange 3.1 and 3.2

--- src/task3_1.c
+++ src/task3_1.c
@@ -1,9 +1,15 @@
-//Develop a program that receives a real number as input and prints the number to the console.
+//Develop a program that receives a real number and precision as input and prints the number 
+//with the specified precision to the console.
 #include<stdio.h>
+#include<math.h>
 
 int main(){
-    float a;
-    scanf("%f",&a);
-    printf("%f",a);
+    double a;
+    scanf("%lf",&a);
+    int dem = 0;
+    while(a * pow(10,dem) != (long long)(a * pow(10,dem))){
+        dem++;
+    }
+    printf("%.*lf",dem,a);
     return 0;
 }
--- src/task3_2.c
+++ src/task3_2.c
@@ -1,15 +1,13 @@
 //Develop a program that receives a real number and precision as input and prints the number 
 //with the specified precision to the console.
 #include<stdio.h>
-#include<math.h>
 
 int main(){
     double a;
+    int n;
     scanf("%lf",&a);
-    int dem = 0;
-    while(a * pow(10,dem) != (long long)(a * pow(10,dem))){
-        dem++;
-    }
-    printf("%.*lf",dem,a);
+    scanf("%d",&n);
+    printf("%.*lf",n,a);
+
     return 0;
 }
